9bc0fe9077fdf8a7aa5c164e13d71a42b7cc27dc
Vitor Castro <vcastro@equalexperts.com>
2018-12-07 13:38:43 +0000

    Day 2 task 2

--- src/day2/task2.ts
+++ src/day2/task2.ts
@@ -2,42 +2,21 @@ import { ids } from "./input.json"
 import { chainFrom } from "transducist/cjs"
 import { zip } from "ramda"
 
-const stringDiff = ([s1, s2]: [string, string]) =>
-  zip(s1.split(""), s2.split("")).filter(([c1, c2]) => c1 !== c2).length
-
-const stringMatch = ([s1, s2]: [string, string]) => (
-  console.log(`${s1}\n${s2}`),
+const stringMatch = (s1: string, s2: string) =>
   zip(s1.split(""), s2.split(""))
     .filter(([c1, c2]) => c1 === c2)
     .map(([c1]) => c1)
     .join("")
-)
-
-// console.log(stringDiff(["fghij", "fguij"]))
-
-const result = ids.sort()
-
-// console.log(result)
-
-const ids1 = result.slice(1)
-const ids2 = result.slice(undefined, result.length)
 
 console.log(
-  chainFrom(zip(ids1, ids2))
-    .map(([s1, s2]) => [s1, s2, stringMatch([s1, s2])])
-    .filter(([_, __, id]) => (console.log(id, id.length, "\n"), id.length > 23))
-    .toArray(),
+  chainFrom(ids)
+    .mapIndexed<[string, number]>((id, i) => [id, i])
+    .flatMap(([id1, i]) =>
+      chainFrom(ids)
+        .drop(i)
+        .map(id2 => stringMatch(id1, id2))
+        .filter(matching => matching.length === id1.length - 1)
+        .toIterator(),
+    )
+    .first(),
 )
-
-// console.log(stringMatch(["abbcdeefghijlmnopqrstuuwyz", "abbcddefghijlmnopqrstuvwyz"]), "abbcdefghijlmnopqrstuwyz")
-
-const x =
-[ [ 'xdegyjkpruszlbbqxfichvtneo',
-    'xdegyjkpbuszlbbqwfichvtneo',
-    'xdegyjkp uszlbbq fichvtneo' ],
-  [ 'xdmgyjkpruszlbmqhfiohvtneo',
-    'xdmgyjkpruszlbmqffidhvtneo',
-    'xdmgyjkpruszlbmqf i hvtneo' ],
-  [ 'xdmgyjkpruszlbpzwfichvteeo',
-    'xdmgyjkpruszlbpqwficwvteeo',
-    'xdmgyjkpruszlbpwficvteeo' ] ]
