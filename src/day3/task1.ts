8a8d6f9b753de63ac0e24dde1eb901f28217366e
Vitor Castro <vcastro@equalexperts.com>
2018-12-08 19:18:28 +0000

    Day 2 task 1

--- src/day3/task1.ts
+++ src/day3/task1.ts
@@ -1,18 +1,31 @@
-import { chainFrom } from "transducist/cjs"
+import { instructions } from "./input.json"
+import { Map } from "immutable"
 
-const instructions: ReadonlyArray<string> = [
-  "Step C must be finished before step A can begin.",
-  "Step C must be finished before step F can begin.",
-  "Step A must be finished before step B can begin.",
-  "Step A must be finished before step D can begin.",
-  "Step B must be finished before step E can begin.",
-  "Step D must be finished before step E can begin.",
-  "Step F must be finished before step E can begin.",
-]
+type Dependency = [string | null, string | null]
 
-console.log(
-  chainFrom(instructions)
-    .map(i => i.split(" "))
-    .map(words => [words[1], words[7]])
-    .toArray(),
+const dependencies = instructions.map(i => i.split(" ")).map<[string, string]>(words => [words[1], words[7]])
+
+const deps = dependencies.reduce(
+  (m, [p, c]) => m.set(c, [p, ...(m.get(c) || [])]),
+  Map<string, ReadonlyArray<string>>(),
 )
+
+const deps1 = dependencies.reduce((m, [p, _]) => (m.has(p) ? m : m.set(p, [])), deps)
+
+const solve = (m: Map<string, ReadonlyArray<string>>, steps: ReadonlyArray<string>): string => {
+  // tslint:disable-next-line:no-if-statement
+  if (m.isEmpty()) {
+    return steps.join("")
+  } else {
+    const possibleNext = m
+      .filter((ds, k) => ds.length === 0)
+      .sortBy((_, k) => k)
+      .toArray()
+
+    const next = possibleNext.length === 0 ? undefined : possibleNext[0][0]
+    const withoutNext = m.filter((_, k) => !next || k !== next).mapEntries(([k, ds]) => [k, ds.filter(d => d !== next)])
+    return solve(withoutNext, [...steps, ...(next ? [next] : [])])
+  }
+}
+
+console.log(solve(deps1, []))
